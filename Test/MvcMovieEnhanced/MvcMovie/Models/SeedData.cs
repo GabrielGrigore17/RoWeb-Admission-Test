3993a437b8187bb548aa30016e5cca7ddfa341f0
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-13 10:00:21 +0300

    Finished the ms tutorial

--- Test/MvcMovieEnhanced/MvcMovie/Models/SeedData.cs
+++ Test/MvcMovieEnhanced/MvcMovie/Models/SeedData.cs
@@ -26,6 +26,7 @@ namespace MvcMovie.Models
                         Title = "Home Alone",
                         ReleaseDate = DateTime.Parse("1990-11-16"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -34,6 +35,7 @@ namespace MvcMovie.Models
                         Title = "Home Alone 2: Lost in New York",
                         ReleaseDate = DateTime.Parse("1992-11-20"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -42,6 +44,7 @@ namespace MvcMovie.Models
                         Title = "Home Alone 3",
                         ReleaseDate = DateTime.Parse("1997-12-12"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
 
@@ -49,7 +52,8 @@ namespace MvcMovie.Models
                     {
                         Title = "Home Alone 4: Taking Back the House",
                         ReleaseDate = DateTime.Parse("2002-11-25"),
-                        Genre = "Western",
+                        Genre = "Comedy",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
